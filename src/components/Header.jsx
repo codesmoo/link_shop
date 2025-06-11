4cdc4b4a2273851801ae9d07830c088de91056d5
codesmoo <52499305+codesmoo@users.noreply.github.com>
2025-06-11 15:01:24 +0900

    Fix logo onclick to refresh page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import './Header.css';
 import logo from './../assets/logo.svg';
-import { useNavigate } from 'react-router-dom';
 
 const Header = ({ rightChild }) => {
-  const nav = useNavigate();
   return (
     <header className='Header'>
       <div className='header_logo'>
-        <img src={logo} onClick={() => nav('/list')} />
+        <a href='/list'>
+          <img src={logo} />
+        </a>
       </div>
       <div className='header_right'>{rightChild}</div>
     </header>
